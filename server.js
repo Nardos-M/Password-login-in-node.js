70eeb5a3aff5d1924c5307d8d7f96e624cb000ad
Nardi-M <nardostesfalem688@gmail.com>
2025-12-03 14:56:12 -0700

    login route is now completed,

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.get("/users", (req, res) => {
 // this is why we need to secure our password coz ppl can see or read it
 app.post("/user", async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedpassword = await bcrypt.hash(req.body.title, salt);
+    //const salt = await bcrypt.genSalt(10);
+    const hashedpassword = await bcrypt.hash(req.body.title, 10);
 
-    console.log(salt);
+    //console.log(salt);
     console.log(hashedpassword);
     const user = {
       username: req.body.username,
@@ -39,4 +39,19 @@ app.post("/user", async (req, res) => {
   }
 });
 
+app.post("/users/login", async (req, res) => {
+  const user = users.find((user) => user.username === req.body.username);
+  if (user == null) {
+    return res.status(400).send("can not find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.title, user.title)) {
+      res.send("sucess");
+    } else {
+      res.send("its not allowed");
+    }
+  } catch {
+    res.status(500).send();
+  }
+});
 app.listen(3000);
