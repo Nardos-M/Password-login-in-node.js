ac4e282ce2d31ebfffd1ec894ad01cce3ce7bcbb
Nardi-M <nardostesfalem688@gmail.com>
2025-12-03 14:19:47 -0700

    now the password is hashed i mean the title

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const bcrypt = require("bcrypt");
+
 app.use(express.json());
 
 const users = [
@@ -19,15 +21,22 @@ app.get("/users", (req, res) => {
 });
 
 // this is why we need to secure our password coz ppl can see or read it
-app.post("/user", (req, res) => {
-    const user = {
-        username: req.body.username,
-        title: req.body.title,
-    };
-  console.log(req.body);
+app.post("/user", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedpassword = await bcrypt.hash(req.body.title, salt);
 
-  users.push(user);
-  res.status(201).send();
+    console.log(salt);
+    console.log(hashedpassword);
+    const user = {
+      username: req.body.username,
+      title: hashedpassword,
+    };
+    users.push(user);
+    res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
 });
 
 app.listen(3000);
